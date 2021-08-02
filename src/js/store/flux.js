e7e5ad9f5a9fdd228b903a9ebae437b781a72b07
Mappel21 <Mappel7676@gmail.com>
2021-08-02 19:34:41 +0000

    making store and actions

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,44 @@
+const url = "https://assets.breatheco.de/apis/fake/contact/agenda";
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
-			//(Arrow) Functions that update the Store
-			// Remember to use the scope: scope.state.store & scope.setState()
+			loadContact() {
+				fetch(url + "agenda/downtown_xii")
+					.then(Response => Response.json())
+					.then(result => {
+						console.log("get Contact", result),
+							setStore({
+								contacts: result
+							});
+					})
+					.catch(e => console.error);
+			}
+		},
+			addContact(name, phone, email, address) {
+				fetch(url, {
+					method: "post",
+					headers: {"content-type":"application/json"},
+					body: JSON.stringify({
+						full_name: name,
+						phone: phone,
+						address: address,
+						email: email,
+						agenda_slug: "downtown_xii"
+					})
+				}).then(()=>{
+					fetch (url + "agenda/downtown_xii")
+					.then(Response=> Response.json())
+					.then(result => {
+						console.log("result", result),
+						setStore({
+							contacts: result
+						});
+					}) .catch(e=> console.error(e))
+				})
+
 		}
 	};
 };
